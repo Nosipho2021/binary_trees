0590945534bc30572a69dcf923741c1ca2c40407
Nosipho2021 <lorrainemdk@gmail.com>
2024-04-01 06:04:01 -0700

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,27 +15,27 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    /* Check if parent is NULL */
-    if (parent == NULL)
-        return (NULL);
+	/* Check if parent is NULL */
+	if (parent == NULL)
+	return (NULL);
 
-    /* Allocate memory for new node */
-    new_node = malloc(sizeof(binary_tree_t));
-    if (new_node == NULL)
-        return (NULL);
+	/* Allocate memory for new node */
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	return (NULL);
 
-    /* Assign values to the new node */
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
+	/* Assign values to the new node */
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
 
-    /* Inserting as right child */
-    new_node->right = parent->right;
-    if (new_node->right != NULL)
-        new_node->right->parent = new_node;
-    parent->right = new_node;
+	/* Inserting as right child */
+	new_node->right = parent->right;
+	if (new_node->right != NULL)
+	new_node->right->parent = new_node;
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
