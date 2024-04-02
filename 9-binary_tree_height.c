4e230de72f7c5d5a20204722d57be5a08a33274f
Nosipho2021 <lorrainemdk@gmail.com>
2024-04-02 01:00:17 -0700

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,19 +8,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    /* Check if tree is NULL */
-    if (tree == NULL)
-        return (0);
+	/* Check if tree is NULL */
+	if (tree == NULL)
+	return (0);
 
-    /* Get the height of the left subtree */
-    left_height = binary_tree_height(tree->left);
+	/* Get the height of the left subtree */
+	left_height = binary_tree_height(tree->left);
 
-    /* Get the height of the right subtree */
-    right_height = binary_tree_height(tree->right);
+	/* Get the height of the right subtree */
+	right_height = binary_tree_height(tree->right);
 
-    /* Return the maximum height of the subtrees + 1 (to account for the current node) */
-    return (1 + MAX(left_height, right_height));
+	/* Return the maximum height of the subtrees + 1 (to account for the current node) */
+	return (1 + MAX(left_height, right_height));
 }
 
